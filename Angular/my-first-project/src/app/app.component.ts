cd7ca68cb0519af9b50ff7929e5942ed4fe7146a
Claud <claudiocmjbio@gmail.com>
2022-07-12 21:01:23 -0300

    first commit

--- Angular/my-first-project/src/app/app.component.ts
+++ Angular/my-first-project/src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeopleService } from './shared/services/people.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-first-project';
+export class AppComponent implements OnInit{
+  count = 0;
+  nome = 'Claudio Montenegro';
+  text = '';
+
+  pessoas = [{
+    firstName: '',
+    lastName: '',
+    age: ''
+  }
+
+  ];
+
+  constructor (private peopleService: PeopleService) { 
+    //injetando meu serviço (PeopleService) no constructor
+    
+  }
+
+
+  ngOnInit() {
+    this.getPeople();
+    let interval = setInterval(() => {
+      this.count++;
+      if(this.count === 10) {
+        clearInterval(interval); 
+      }
+    }, 1000)
+  }
+
+  getPeople() {
+    this.peopleService.getPeople().subscribe(people => {
+      this.pessoas = people;
+    })
+  }
+
+  clicou(nome: string): void{
+    console.log('Clicou em mim', nome)
+  }
 }
--- Angular/my-first-project/src/app/app.module.ts
+++ Angular/my-first-project/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; 
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { PeopleService } from './shared/services/people.service';
+import { MarcaTextoDirective } from './shared/diretivas/marca-texto.directive';
+import { ListPeopleComponent } from './shared/components/list-people/list-people.component';
+import { ListAPIComponent } from './shared/components/list-api/list-api.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarcaTextoDirective,
+    ListPeopleComponent,
+    ListAPIComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PeopleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
